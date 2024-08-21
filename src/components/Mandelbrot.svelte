f4faba663bfd904c06177fd32955b46a302c68a3
Pedro Alves <pedron18@uia.no>
2024-08-22 00:35:44 +0200

    working

--- src/components/Mandelbrot.svelte
+++ src/components/Mandelbrot.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    const max_iterations = 1000;
+    const max_iterations = 100;
     const n = 2;
     const c = [-1, 0]
     const R = (1 + Math.sqrt(1+4*abs(c))) / 2; // escape radius choose R > 0 such that R**n - R >= sqrt(cx**2 + cy**2)
@@ -9,7 +9,7 @@
     }
 
     function julia(x: number, y: number, width: number, height: number): number {
-        /* returns a number [0, max_iterations] */
+        /* returns a number [0, R] */
 
         let m = R / width;
         let x1 = m * (2 * x - width);
@@ -17,42 +17,47 @@
 
         let z =  [x1, y2];
 
+        let zs = [abs(z)];
+
         let i = 0;
         while (i < max_iterations) 
         {
             z = [z[0]*z[0] - z[1] * z[1] + c[0], 2 * z[0] * z[1] + c[1]];
             if (abs(z) > R) return 0;
 
+            zs.push(abs(z));
+
             i += 1;
         }
 
-        return i;
+        return zs.reduce((a, b) => a + b, 0) / zs.length;
     }
 
     function draw(event: Event) {
-        console.clear();
+        console.log({R})
         let canvas = document.getElementById("canvas");
         let ctx = canvas.getContext("2d");
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        console.log(ctx);
-        //console.log(R**n - R >= Math.sqrt(cx**2 + cy**2)); // this must be true see up
-        console.log(event.clientX)
-        console.log(event.clientY)
 
-        // scale them down
-        let mousePosX = 0;
-        let mousePosY = 0.5;
+        let mousePosX = event.clientX;
+        let mousePosY = event.clientY;
 
         for (let x = 0; x <= canvas.width; x++) {
             for (let y = 0; y <= canvas.height; y++) {
-                let iter_nr = julia(x, y, canvas.width, canvas.height);
+                let avg_growth = julia(x, y, canvas.width, canvas.height);
 
                 //const blueIntensity = Math.round((result / max_iterations) * 255);
-                if(iter_nr > 0)
+                if(avg_growth > 0)
                 {
-                    const blueIntensity = Math.round((iter_nr / max_iterations) * (255*(1/3)));
-                    ctx.fillStyle = `rgb(0, 0, ${blueIntensity})`;
+                    const scaleIntensity = Math.exp(avg_growth / R) - 1;
+                    const colorIntensity = Math.round(scaleIntensity * 255);
+                    console.log({scaleIntensity});
+                    if(scaleIntensity > 0.4)
+                        ctx.fillStyle = `rgb(0, ${colorIntensity}, 0)`;
+                    else{
+                        ctx.fillStyle = `rgb(0, 0, ${colorIntensity})`;
+                    }
                     ctx.fillRect(x, y, 1, 1);
                 }
             }
