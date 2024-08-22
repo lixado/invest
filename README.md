463858601f0ebc949abef5f13396ec2e87a42a13
Pedro Alves <pedron18@uia.no>
2024-08-22 22:43:20 +0200

    working julia set

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ https://github.com/illumincrotty/Circular-Hue-Color-Picker/blob/main/lib/index.t
 ## TODO
 - finish circle chooser and move it as a component
 - make background based on mouse position & mandelbrot set
+- https://medium.com/@nomanfareed681/parallel-processing-in-javascript-with-concurrency-c214e9facefd
 
 
 # Svelte + TS + Vite
--- src/components/Mandelbrot.svelte
+++ src/components/Mandelbrot.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
-    const max_iterations = 100;
-    const n = 2;
-    const c = [-1, 0]
-    const R = (1 + Math.sqrt(1+4*abs(c))) / 2; // escape radius choose R > 0 such that R**n - R >= sqrt(cx**2 + cy**2)
+    import { onMount } from "svelte";
 
-    function abs(z){  // absolute value of a complex number
+    const max_iterations = 25;
+    const n = 2;
+    const minCx = -1;
+    const maxCx = 0.4;
+    const minCy = -1;
+    const maxCy = 1;
+    const mouseEventsSkip = 5; // the lower the better FPS, but the lower the more cpu we use and might get laggy
+
+    var c = [-1, 0]
+    var R = (1 + Math.sqrt(1+4*abs(c))) / 2; // escape radius
+    var currentMouseEvent = 0;
+    var context: CanvasRenderingContext2D;
+
+    function abs(z: Array<number>){  // absolute value of a complex number
         return Math.sqrt(z[0]*z[0] + z[1]*z[1]);
     }
 
     function julia(x: number, y: number, width: number, height: number): number {
-        /* returns a number [0, R] */
+        /* returns a number [0, 1] */
 
-        let m = R / width;
-        let x1 = m * (2 * x - width);
-        let y2 = m * (height - 2 * y);
+        let x1 = (R/width) * (2 * x - width);
+        let y2 = (R/height) * (height - 2 * y);
 
         let z =  [x1, y2];
 
@@ -33,40 +42,75 @@
         return zs.reduce((a, b) => a + b, 0) / zs.length;
     }
 
-    function draw(event: Event) {
-        console.log({R})
-        let canvas = document.getElementById("canvas");
-        let ctx = canvas.getContext("2d");
+    function toHex( val: number ) {
+      return  Math.round( val * 255 ).toString(16);
+    }
 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    function draw(event: MouseEvent) {
+        if(currentMouseEvent < mouseEventsSkip)
+        {
+            currentMouseEvent += 1;
+            return;
+        }
+        else { currentMouseEvent = 0; }
 
-        let mousePosX = event.clientX;
-        let mousePosY = event.clientY;
+        // get dims
+        const width = window.innerWidth;
+        const height = window.innerHeight;
 
-        for (let x = 0; x <= canvas.width; x++) {
-            for (let y = 0; y <= canvas.height; y++) {
-                let avg_growth = julia(x, y, canvas.width, canvas.height);
+        // clear
+        context.clearRect(0, 0, width, height);
 
-                //const blueIntensity = Math.round((result / max_iterations) * 255);
+        // transform it to [minCx, maxCx]
+        let mousePosX = minCx + (maxCx - minCx)*(event.clientX/width);
+        let mousePosY = minCy + (maxCy - minCy)*(event.clientY/height);
+
+        c = [mousePosX, mousePosY];
+        R = (1 + Math.sqrt(1+4*abs(c))) / 2;
+
+        let img = new ImageData( width, height );
+        let img_data = new Uint32Array( img.data.buffer );
+
+        for (let x = 0; x <= width; x++) 
+        {
+            for (let y = 0; y <= height; y++) 
+            {
+                const scale = (1/7);
+                let avg_growth = julia(x, y, width*scale, height*scale); // moving it into place
+
                 if(avg_growth > 0)
                 {
-                    const scaleIntensity = Math.exp(avg_growth / R) - 1;
+                    const scaleIntensity = Math.exp(avg_growth / (R)) - 1;
                     const colorIntensity = Math.round(scaleIntensity * 255);
-                    console.log({scaleIntensity});
-                    if(scaleIntensity > 0.4)
-                        ctx.fillStyle = `rgb(0, ${colorIntensity}, 0)`;
-                    else{
-                        ctx.fillStyle = `rgb(0, 0, ${colorIntensity})`;
-                    }
-                    ctx.fillRect(x, y, 1, 1);
+
+                    let rgbNumber = Number( '0xFF' + toHex(colorIntensity) + toHex(0) + toHex(0) ); // we want 0xAABBGGRR format
+
+                    if(scaleIntensity > 0.4) rgbNumber = Number( '0xFF' + toHex(0) + toHex(colorIntensity) + toHex(0) );
+                    
+                    img_data[ y * width + x] = rgbNumber; // save to buffer
+                }
+                else
+                {
+                    img_data[ y * width + x] = Number( '0xFF' + toHex(0) + toHex(0) + toHex(0) ); // black pixel
                 }
             }
         }
+
+        // draw now everything at same time
+        context.putImageData(img, 0, 0 );
     }
+
+    onMount(() => {
+        let canvas = <HTMLCanvasElement>document.getElementById("canvas");
+
+        context = canvas.getContext("2d") ?? new CanvasRenderingContext2D;
+        context.imageSmoothingEnabled = true;
+        context.imageSmoothingQuality = "high";
+    });
 </script>
 
 <main>
-    <canvas class="fillViewPort" id="canvas" on:click={draw}></canvas>
+    <canvas class="fillViewPort" id="canvas" on:mousemove={draw}></canvas>
 </main>
 
 <style>
